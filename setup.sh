c0120a71692745b4ae15bdb63bfc68d7b1524153
Josiah Hunsinger <jhunsinger2860@flpoly.org>
2016-10-27 09:01:49 -0400

    initial commit

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Use the first argument to this script to set our hostname
+if [ -z $1 ]; then
+   echo "Please provide a new hostname as the first argument to this script."
+   exit
+fi
+OLD_HOSTNAME=$(hostname)
+echo $1 > /etc/hostname
+sed -i "s/$OLD_HOSTNAME/$1/g" /etc/hosts
+
+# Install packages
+apt-get install git batctl
+
+# Download and install proto-mesh
+cd /home/pi
+git clone https://github.com/aiguy1010/proto-mesh.git
+cd proto-mesh
+bash ./setup.sh
+
+
